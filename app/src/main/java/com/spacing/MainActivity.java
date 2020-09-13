3adf7858a3dd651863028b984686788229313924
Rahul Srivastava <rahul.srivastava@sierradev.com>
2020-09-13 10:18:23 +0530

    Ui optimization

--- app/src/main/java/com/spacing/MainActivity.java
+++ app/src/main/java/com/spacing/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.looking_for_dialog);
+        setContentView(R.layout.verify_number_or_skip);
     }
 }
